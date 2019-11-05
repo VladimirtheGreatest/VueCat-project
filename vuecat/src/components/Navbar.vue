4b17eb883c8be60cb9dc1b92c21c1f4577c389fd
VladimirtheGreatest <vladkodoktorov@gmail.com>
2019-11-05 18:16:50 +0000

    navigation bar, the first component, installing dependencies, testing the first api request

---
+++ vuecat/src/components/Navbar.vue
@@ -0,0 +1,48 @@
+<template>
+  <nav>
+         <!--navbar-->
+    <v-app-bar app class="purple accent-2">
+         <v-app-bar-nav-icon class="black--text" @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title class="text-uppercase">
+        <span class="font-weight-light">Vue</span>
+        <span>Cat</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn text>
+          <v-icon left>apps</v-icon>
+        <span color="black">Log in</span>
+             <!--navbar top end-->
+      </v-btn>
+    </v-app-bar>
+          <!--navbar links-->
+     <v-navigation-drawer left app v-model="drawer" class="purple accent-2">
+        <v-list>
+        <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
+          <v-list-item-icon>
+            <v-icon class="black--text">{{ link.icon }}</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title class="black--text">{{ link.text }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+</v-list>
+    </v-navigation-drawer>
+     <!--navbar links end-->
+
+  </nav>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    drawer: false,
+     links: [
+        { icon: 'mood', text: 'Cats', route: '/' },
+        { icon: 'group_work', text: 'Breeds', route: '/Breeds' },
+        { icon: 'category', text: 'Categories', route: '/categories' }
+      ]
+    }
+  }
+}
+</script>
--- vuecat/src/views/Home.vue
+++ vuecat/src/views/Home.vue
@@ -1,18 +1,65 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+       <v-container fluid >
+              <v-row >
+                <v-col class="mb-5" sm="12" md="12" xs="12">
+                  <v-layout class="mb-5" align-center justify-center>
+                     <v-btn color="pink" dark large @click="loadNextImage" >
+                    See another cutie &nbsp; <v-icon>refresh</v-icon>
+                   </v-btn>
+                    </v-layout>
+
+                   <v-layout row wrap>
+                     <v-flex xs12 >
+                        <v-progress-circular v-if="loading" :size="100" indeterminate color="pink"></v-progress-circular>
+                       <v-card v-else class="mx-auto mb-5" max-width="900" height="700" outlined elevation="24" rounded >
+                        <v-img :src="image.url" max-width="900" height="700">
+                        </v-img>
+                        </v-card>
+                       </v-flex>
+                       </v-layout>
+                     </v-col>
+                    </v-row>
+       </v-container>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import Swal from 'sweetalert2'
+import axios from 'axios';
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
+  data () {
+    return {
+          image: { url: ""},
+             loading : true
+    }
+  },
+        created(){
+            this.loadNextImage();
+        } ,
+        methods:{
+            async loadNextImage() {
+                try{  
+                    Swal.fire({
+                      position: 'center',
+                      icon: 'success',
+                      title: 'MEOOOOOOWWWWWWW',
+                      showConfirmButton: false,
+                      timer: 2000
+                    })
+                    axios.defaults.headers.common['x-api-key'] = "91a07ee8-70f3-4a3d-9296-0c6ca54428ef" 
+                    let response = await axios.get('https://api.thecatapi.com/v1/images/search', { params: { limit:1, size:"full" } } ) // Ask for 1 Image, at full resolution
+                    this.image = response.data[0] // the response is an Array, so just use the first item as the Image
+                    console.log("-- Image from TheCatAPI.com")
+                    console.log("id:", this.image.id)
+                    console.log("url:", this.image.url)
+                    console.log(this.image)
+                       this.loading = false
+                }catch(err){
+                    console.log(err)
+                    this.loading = false
+                }
+            }
+        }
 }
 </script>
