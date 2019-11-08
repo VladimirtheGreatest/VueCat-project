1db56d034579bda7d0095dda22c12ec3790597aa
Vladimir <48209956+VladimirtheGreatest@users.noreply.github.com>
2019-11-08 10:59:11 +0000

    fix for the i-phone 5 media query

--- vuecat/src/views/Home.vue
+++ vuecat/src/views/Home.vue
@@ -12,7 +12,7 @@
               <v-row >
                 <v-col class="mb-5" sm="12" md="12" xs="12">
                   <v-layout class="mb-5" align-center justify-center>
-                     <v-btn class="pink font-weight-bold my-12" dark large @click="loadNextImage" >
+                     <v-btn class="pink font-weight-bold mt-12" dark large @click="loadNextImage" >
                     See another cutie &nbsp; <v-icon>refresh</v-icon>
                    </v-btn>
                     </v-layout>
@@ -20,8 +20,8 @@
                    <v-layout>
                      <v-flex xs12 >
                         <v-progress-circular v-if="loading" :size="100" indeterminate color="pink"></v-progress-circular>
-                       <v-card v-else class="mx-auto mb-5" max-width="900" height="600" outlined elevation="24" rounded >
-                        <v-img id="card" :src="image.url" max-width="900" height="600">
+                       <v-card v-else class="mx-auto mb-5" max-width="800" max-height="500" outlined elevation="24" rounded >
+                        <v-img id="card" :src="image.url" max-width="800" max-height="500">
                         </v-img>
                         </v-card>
                        </v-flex>
@@ -47,9 +47,9 @@ export default {
         } ,
         methods:{
             async loadNextImage() {
-                try{  
+                try{
                     Swal.fire({
-                    title: 'fetching from API....meowwwwww',
+                    title: 'fetching from API..meoww',
                       position: 'top',
                     width: 600,
                     padding: '3em',
@@ -61,12 +61,12 @@ export default {
                       url("https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif")
                       center
                       no-repeat
-                      
+
                     `
                   })
-                    axios.defaults.headers.common['x-api-key'] = "91a07ee8-70f3-4a3d-9296-0c6ca54428ef" 
+                    axios.defaults.headers.common['x-api-key'] = "91a07ee8-70f3-4a3d-9296-0c6ca54428ef"
                     let response = await axios.get('https://api.thecatapi.com/v1/images/search', { params: { limit:1, size:"full" } } ) // Ask for 1 Image, at full resolution
-                    this.image = response.data[0]               
+                    this.image = response.data[0]
                        this.loading = false
                 }catch(err){
                     console.log(err)
@@ -79,10 +79,18 @@ export default {
 
  <style scoped>
   #card{
-    -webkit-box-shadow: 5px 5px 44px 39px rgba(255,105,180,0.57); 
+    -webkit-box-shadow: 5px 5px 44px 39px rgba(255,105,180,0.57);
     box-shadow: 5px 5px 44px 39px rgba(255,105,180,0.57);
   }
   #huge{
       font-size: 42px;
+  }
+  @media (max-width: 480px) {
+    #huge{
+      font-size: 35px;
+    }
+    #card{
+    max-height: 300px;
+    }
   }
   </style>
